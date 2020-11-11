69171421b819b12ec5bd58b4c05390fd8b2c83ee
Xiaoshuai Geng <geng115@uwindsor.ca>
2020-11-10 21:04:54 -0500

    update

--- Game.py
+++ Game.py
@@ -1,6 +1,6 @@
 class Game:
     def __init__(self, store_id=None, name='Default Name', release_date='2000-1-1',
-                 genre='Unknown', platform='Unknown', price=0.0):
+                 genre=list('Unknown'), platform='Unknown', price=0.0):
         self.id = store_id
         self.name = name
         self.release_date = release_date
@@ -9,9 +9,33 @@ class Game:
         self.price = price
 
     # setter / getter
-    def set_id(self, store_id):
+
+    # set_id shouldnt be allowed to use because id is
+    # an auto-increament value
+    def __set_id__(self, store_id: int):
         self.id = store_id
 
     def get_id(self):
         return self.id
 
+    def set_name(self, name: str):
+        self.name = name
+
+    def get_name(self) -> str:
+        return self.name
+    
+    def set_date(self, date: str):
+        self.release_date = date
+
+    def get_date(self) -> str:
+        return release_date
+    
+    def set_genre(self, genre: list) :
+        self.genre = genre
+    
+
+    def add_genre(self, tag: str):
+        self.genre.append(tag)
+
+    
+    
